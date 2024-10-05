f7f03af2e27dd3608379954179a2168eca13cee5
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2024-10-05 11:43:47 +0100

    Initial Commmit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/doctors' element={<Doctors />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/doctors/:speciality' element={<Doctors />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/my-appointments' element={<MyAppointments />}/>
+        <Route path='/appointment/:docId' element={<Appointment />}/>
+        <Route path='/my-profile' element={<MyProfile />}/>
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
