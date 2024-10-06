5089660c9fce27633c0b3955eebd67e365f49e4e
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2024-10-06 18:07:10 +0100

    Created the contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -7,8 +7,17 @@ const Contact = () => {
       <div className='text-center text-2xl pt-10 text-gray-500'>
         <p>CONTACT <span className='text-gray-700 font-semibold'>US</span> </p>
       </div>
-      <div>
-        <img src={assets.contact_image} alt="" />
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+      
+      <div className='flex flex-col justify-center gap-6'>
+        <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+        <p className='text-gray-500'>60, <br />OSinfolarin AKoka</p>
+        <p className='text-gray-500'>8021173127</p>
+        <p className='text-gray-500'>ebenezerojeab@gmail.com</p>
+        <p className='font-semibold text-lg text-gray-600'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, tenetur.</p>
+        <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all'>Explore Jobs</button>
+      </div>
       </div>
     </div>
   )
