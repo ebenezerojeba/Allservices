e5c774607881fde4149201ba04221c6251697e0c
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2024-10-25 17:47:31 +0100

    Modified the cntact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -35,22 +35,18 @@ const Contact = () => {
     setFormData({ name: "", email: "", message: "" });
     setRecordedAudio(null);
   };
-  const toggleRecording = () => {
-    if (isRecording) {
-      setIsRecording(false);
-      setRecordedAudio(new Blob());
-    } else {
-      setIsRecording(true);
-    }
-  };
+  // const toggleRecording = () => {
+  //   if (isRecording) {
+  //     setIsRecording(false);
+  //     setRecordedAudio(new Blob());
+  //   } else {
+  //     setIsRecording(true);
+  //   }
+  // };
 
   return (
     <div
-      className={`min-h-screen py-8 sm:py-12 px-4 sm:px-6 lg:px-8 transition-colors duration-300 ${
-        theme === "light"
-          ? "bg-gray-100 text-black"
-          : "bg-gradient-to-br from-gray-900 via-purple-900 to-violet-900 text-white"
-      }`}
+      className= 'min-h-screen py-8 sm:py-12 px-4 sm:px-6 lg:px-8 transition-colors duration-300 '
     >
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col sm:flex-row justify-between items-center mb-8 sm:mb-12">
@@ -61,30 +57,29 @@ const Contact = () => {
             onClick={toggleTheme}
             className="px-3 py-1 sm:px-4 sm:py-2 rounded-full bg-opacity-20 backdrop-blur-lg bg-white text-gray-800 dark:bg-gray-800 dark:text-white shadow-lg hover:shadow-xl transition-all duration-300"
           >
-            {theme === "light" ? "🌙" : "☀️"}
           </button>
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-12">
-          <div className="bg-white text-gray-900 dark:text-white dark:bg-gray-900 rounded-lg shadow-lg p-6 transition-all duration-300 hover:shadow-2xl transform hover:-translate-y-1">
-            <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold text-gray-700 dark:text-white mb-4 sm:mb-6">
+          <div className="bg-white text-gray-900 dark:text-white dark:bg-gray-600 rounded-lg shadow-lg p-6 transition-all duration-300 hover:shadow-2xl transform hover:-translate-y-1">
+            <h2 className="text-xl sm:text-xl md:text-2xl font-medium text-gray-700 dark:text-white mb-4 sm:mb-6">
               Contact Information
             </h2>
             <div className="space-y-4 text-sm sm:text-base">
               <div className="flex items-center space-x-4 text-gray-600 dark:text-gray-300">
-                <MapPin className="text-purple-600 dark:text-blue-900 flex-shrink-0" />
+                <MapPin className="text-purple-600 w-5 dark:text-blue-900 flex-shrink-0" />
                 <span>60, Osinfolarin Akoka, Lagos, Nigeria</span>
               </div>
               <div className="flex items-center space-x-4 text-gray-600 dark:text-gray-300">
-                <Phone className="text-purple-600 dark:text-blue-900 flex-shrink-0" />
+                <Phone className="text-purple-600 w-5 dark:text-blue-900 flex-shrink-0" />
                 <span>+234 802 117 3127</span>
               </div>
               <div className="flex items-center space-x-4 text-gray-600 dark:text-gray-300">
-                <Mail className="text-purple-600 dark:text-blue-900 flex-shrink-0" />
+                <Mail className="text-purple-600 w-5 dark:text-blue-900 flex-shrink-0" />
                 <span className="break-all">contact@skillink.com</span>
               </div>
               <div className="flex items-center space-x-4 text-gray-600 dark:text-gray-300">
-                <Clock className="text-purple-600 dark:text-blue-900 flex-shrink-0" />
+                <Clock className="text-purple-600 w-5 dark:text-blue-900 flex-shrink-0" />
                 <span>Monday - Friday: 9am - 5pm</span>
               </div>
             </div>
@@ -93,12 +88,12 @@ const Contact = () => {
                {/* Interactive Map */}
         <div className="mt-4 bg-white dark:bg-gray-800 rounded-lg shadow-lg p-5 transition-all duration-300 hover:shadow-2xl sm:mt-4">
           <div className="flex justify-between items-center mb-4">
-            <h2 className="text-2xl font-semibold text-gray-800 dark:text-white ">
+            <h2 className="text-2xl font-meidum text-gray-800 dark:text-white ">
               Find Us
             </h2>
             <button
               onClick={() => setShowMap(!showMap)}
-              className="px-4 py-2 rounded-full bg-purple-600 text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 transition-colors duration-300"
+              className="px-4 py-2 rounded-full bg-gray-500 text-white hover:bg-purple-00 focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors duration-300"
             >
               {showMap ? "Hide Map" : "Show Map"}
             </button>
@@ -116,8 +111,8 @@ const Contact = () => {
           )}
         </div>
 
-          <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 transition-all duration-300 hover:shadow-2xl transform hover:-translate-y-1">
-            <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold text-gray-800 dark:text-white mb-4 sm:mb-6">
+          <div className="bg-white dark:bg-gray-600 rounded-lg shadow-lg p-6 transition-all duration-300 hover:shadow-2xl transform hover:-translate-y-1">
+            <h2 className="text-xl sm:text-2xl md:text-3xl font-medium text-center text-gray-800 dark:text-white mb-4 sm:mb-6">
               Send Us a Message
             </h2>
             <form onSubmit={handleSubmit} className="space-y-4">
@@ -135,7 +130,7 @@ const Contact = () => {
                   value={formData.name}
                   onChange={handleChange}
                   required
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-purple-500 focus:ring-purple-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white text-sm sm:text-base"
+                  className="p-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-gray-500 focus:gray-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white text-sm sm:text-base"
                 />
               </div>
               <div>
@@ -152,7 +147,7 @@ const Contact = () => {
                   value={formData.email}
                   onChange={handleChange}
                   required
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-purple-500 focus:ring-purple-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white text-sm sm:text-base"
+                  className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-purple-500 focus:ring-purple-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white text-sm sm:text-base"
                 />
               </div>
               <div>
@@ -179,12 +174,12 @@ const Contact = () => {
               <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
                 <button
                   type="submit"
-                  className="inline-flex items-center justify-center px-4 py-2 w-full sm:w-auto border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 transition-colors duration-300"
+                  className="inline-flex items-center justify-center px-4 py-2 w-full sm:w-auto border border-transparent text-sm font-medium rounded-xl shadow-sm text-black bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2  transition-colors duration-300"
                 >
-                  <Send className="mr-2 h-5 w-5" />
+                  
                   Send Message
                 </button>
-                <button
+                {/* <button
                   type="button"
                   onClick={toggleRecording}
                   className={`inline-flex items-center justify-center px-4 py-2 w-full sm:w-auto border border-transparent text-sm font-medium rounded-md shadow-sm text-white ${
@@ -204,7 +199,7 @@ const Contact = () => {
                       Record Message
                     </>
                   )}
-                </button>
+                </button> */}
               </div>
               {recordedAudio && (
                 <audio controls className="mt-4 w-full">
